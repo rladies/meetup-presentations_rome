ba170533fb97c47470630fd87641720c9f1785e6
fgazzelloni <docksbox@pm.me>
2023-05-17 23:55:14 +0200

    adding May talk

--- organisersKit/logo/logo_maker.R
+++ organisersKit/logo/logo_maker.R
@@ -9,7 +9,7 @@
 # ## Automatically use showtext to render text for future devices
 # showtext_auto()
 
-setwd("R-Ladies/RLadiesRome/logo")
+setwd("~/Documents/R/R_general_resources/R-Ladies/RLadiesRome/meetup-presentations_rome/organisersKit/logo")
 
 # Load necessary libraries
 library(ggplot2)
@@ -17,30 +17,31 @@ library(cowplot)
 
 # Start drawing the logo
 p <- ggdraw()+
-  draw_image("baseRlogo.png",scale=2.2,y=0.43)+
+  draw_image("baseRlogo-rmbg.png",scale=2.2,y=0.43)+
   draw_image("coloseum.png",scale=0.3,y=0.8,x=0.1)
-  
+
 ## Make the hexSticker
-hexSticker::sticker(p, 
-                    package="Ladies Rome", 
+hexSticker::sticker(p,
+                    package="Ladies Rome",
                     p_x = 1,
                     p_y = 0.48,
-                    p_size=18, 
-                    s_x=1, 
-                    s_y=.75, 
-                    s_width=1.5, 
+                    p_size=18,
+                    s_x=1,
+                    s_y=.75,
+                    s_width=1.5,
                     s_height=1,
-                    p_color = "#88398a", 
-                    h_fill = "white", 
+                    p_color = "#88398a",
+                    h_fill = "white",
                     h_color = "#88398a",
                     dpi = 320,
-                    #p_family = "", 
+                    #p_family = "",
+                    white_around_sticker = FALSE,
                     filename="base_logo.png")
 
 # this reduces the size of the image to fit on Twitter:
 ggdraw()+
   draw_image("base_logo.png")
-ggsave("logo.png", width = 4.5,height = 4.5)
+ggsave("logo.png", width = 4.5,height = 4.5,bg=NA)
 
 # magick::image_read("logo.png")
 # rstudioapi::restartSession()
